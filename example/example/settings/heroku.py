929ccafe3545019388fba6eefca2fd2e01215d42
Arkadiusz Adamski <arkadiusz.adamski@allegro.pl>
2019-01-11 21:24:48 +0100

    Add ALLOWED_HOSTS for heroku

--- example/example/settings/heroku.py
+++ example/example/settings/heroku.py
@@ -1,6 +1,7 @@
 from . import *  # noqa
 import dj_database_url
 
+ALLOWED_HOSTS = ['django-dedal.herokuapp.com']
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
