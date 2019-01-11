7964a804c25b4a1751c5888097d3c2cdb43f624d
Arkadiusz Adamski <arkadiusz.adamski@allegro.pl>
2019-01-11 21:33:48 +0100

    Modify example settings

--- example/example/settings/__init__.py
+++ example/example/settings/__init__.py
@@ -42,15 +42,14 @@ INSTALLED_APPS = (
     'bootstrapform',
 )
 
-MIDDLEWARE_CLASSES = (
+MIDDLEWARE = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 )
 
 ROOT_URLCONF = 'example.urls'
