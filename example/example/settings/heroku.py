343d71278b57ae2ec1997cdddd1b31d5507fb3ee
Arkadiusz Adamski <ar4s@users.noreply.github.com>
2020-12-19 13:41:23 +0100

    Update heroku.py

--- example/example/settings/heroku.py
+++ example/example/settings/heroku.py
@@ -2,7 +2,7 @@ from . import *  # noqa
 import dj_database_url
 
 SECURE_SSL_REDIRECT = True
-ALLOWED_HOSTS = ['django-dedal.herokuapp.com', 'django-dedal.source.net.pl']
+ALLOWED_HOSTS = ['*']
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
